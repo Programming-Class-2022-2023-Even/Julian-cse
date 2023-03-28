079135825117da91af54c9c2e63b02d9cca62d04
julian57677 <58318502+julian57677@users.noreply.github.com>
2023-03-28 08:32:58 +0700

    Add files via upload

---
+++ Assigment1/Assigment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+        Console.WriteLine("Output 1 (row-wise): ");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+        }
+
+        Console.WriteLine("\nOutput 2 (column-wise): ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+        }
+    }
+}
